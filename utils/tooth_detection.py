0649bd13ed2e9427060d1cf9107dbb625f5c6e7a
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:38:07 +0530

    Update tooth_detection.py

--- utils/tooth_detection.py
+++ utils/tooth_detection.py
@@ -1,1 +1,18 @@
 # tooth detection logic
+import cv2
+
+def detect_teeth(mask):
+
+    contours,_ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+    teeth_boxes=[]
+
+    for c in contours:
+
+        x,y,w,h = cv2.boundingRect(c)
+
+        if w*h>500:
+
+            teeth_boxes.append((x,y,w,h))
+
+    return teeth_boxes
