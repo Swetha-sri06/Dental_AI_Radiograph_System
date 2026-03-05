022edbef8a07607f0842cc06bc24347a7bfd50ba
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:37:47 +0530

    Update train_segmentation.py

--- train_segmentation.py
+++ train_segmentation.py
@@ -1,1 +1,11 @@
 # training script for UNet segmentation
+import torch
+from models.unet_model import get_unet
+
+model = get_unet()
+
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-4)
+
+loss_fn = torch.nn.BCEWithLogitsLoss()
+
+print("Segmentation model ready")
