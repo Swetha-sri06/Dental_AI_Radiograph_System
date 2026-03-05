15266d55be56ad32c7cef30acc05f45da32aab99
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:39:19 +0530

    Update classifier.py

--- models/classifier.py
+++ models/classifier.py
@@ -1,1 +1,11 @@
 # Tooth condition classifier
+import torchvision.models as models
+import torch.nn as nn
+
+def get_classifier():
+
+    model = models.efficientnet_b0(pretrained=True)
+
+    model.classifier[1] = nn.Linear(1280,4)
+
+    return model
