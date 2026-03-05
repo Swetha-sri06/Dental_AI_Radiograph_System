bed497d045f920f9bca9adef8c8362aabb0a8921
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:39:37 +0530

    Update anomaly.py

--- utils/anomaly.py
+++ utils/anomaly.py
@@ -1,1 +1,18 @@
 # anomaly detection module
+import numpy as np
+
+def detect_anomaly(img):
+
+    mean = np.mean(img)
+
+    if mean < 0.3:
+
+        return "Possible Bone Loss"
+
+    elif mean > 0.7:
+
+        return "Possible Implant"
+
+    else:
+
+        return "Normal"
