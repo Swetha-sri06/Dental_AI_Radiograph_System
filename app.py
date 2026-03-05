005e1e0e0adfe61b18c15afed0271cd2ab216535
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:40:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 # streamlit dashboard
+import streamlit as st
+import cv2
+from inference import run_inference
+
+st.title("AI Dental Radiograph Analysis")
+
+uploaded = st.file_uploader("Upload OPG")
+
+if uploaded:
+
+    file_bytes = uploaded.read()
+
+    with open("temp.png","wb") as f:
+        f.write(file_bytes)
+
+    teeth,numbers = run_inference("temp.png")
+
+    st.image("temp.png")
+
+    st.write("Detected Teeth:",len(teeth))
