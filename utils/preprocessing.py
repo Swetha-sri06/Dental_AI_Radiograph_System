c8517ff5304215df45b92eb6ac107c93489f0951
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:36:56 +0530

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,14 @@
 # image preprocessing functions
+import cv2
+import numpy as np
+
+def preprocess_image(img_path):
+
+    img = cv2.imread(img_path,0)
+    img = cv2.resize(img,(512,512))
+
+    img = cv2.equalizeHist(img)
+
+    img = img/255.0
+
+    return np.expand_dims(img,axis=0)
