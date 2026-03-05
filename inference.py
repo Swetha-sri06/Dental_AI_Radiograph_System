11b9cbb8769eeb2f48b044ee17b62a38eb4c0611
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:39:57 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,1 +1,17 @@
 # prediction pipeline
+import cv2
+from utils.preprocessing import preprocess_image
+from utils.tooth_detection import detect_teeth
+from utils.numbering import assign_fdi_numbers
+
+def run_inference(image_path):
+
+    img = cv2.imread(image_path,0)
+
+    mask = img
+
+    teeth = detect_teeth(mask)
+
+    numbers = assign_fdi_numbers(teeth)
+
+    return teeth,numbers
