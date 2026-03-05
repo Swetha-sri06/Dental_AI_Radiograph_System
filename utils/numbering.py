883461fdbcd89fbf412d22b095dbb3e726daae83
Swetha-sri06 <swethasrit29@gmail.com>
2026-03-05 18:38:23 +0530

    Update numbering.py

--- utils/numbering.py
+++ utils/numbering.py
@@ -1,1 +1,12 @@
 # FDI tooth numbering system
+def assign_fdi_numbers(teeth_boxes):
+
+    teeth_boxes = sorted(teeth_boxes,key=lambda x:x[0])
+
+    numbers=[]
+
+    for i,box in enumerate(teeth_boxes):
+
+        numbers.append(i+1)
+
+    return numbers
